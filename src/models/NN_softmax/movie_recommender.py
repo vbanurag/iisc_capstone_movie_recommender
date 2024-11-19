feat: fix softmax

src/models/NN_softmax/movie_recommender.py
@@ -1,16 +1,19 @@
 from pprint import pprint
+from sklearn.calibration import LabelEncoder
 import tensorflow as tf
 from tensorflow import keras
 from typing import Tuple
 import numpy as np
 
 class MovieRecommenderModel:
-    def __init__(self, n_users: int, n_movies: int, refind_df, n_factors: int = 150):
+    def __init__(self, n_users: int, n_movies: int, refind_df, user_enc, movie_enc, n_factors: int = 150):
         self.n_users = n_users
         self.n_movies = n_movies
         self.refined_dataset = refind_df
         self.n_factors = n_factors
         self.model = self.build_model()
+        self.user_enc = user_enc
+        self.movie_enc = movie_enc
 
     def build_model(self) -> keras.Model:
         """Build the neural network architecture"""
@@ -62,8 +65,8 @@ class MovieRecommenderModel:
         
         return model
 
-    def train(self, X_train: Tuple, y_train: np.ndarray, 
-              X_val: Tuple, y_val: np.ndarray, 
+    def train(self, X_train_array: Tuple, y_train: np.ndarray, 
+              X_test_array: Tuple,  Y_test: np.ndarray, 
               epochs: int = 80, batch_size: int = 128) -> keras.callbacks.History:
         """Train the model with callbacks for early stopping and checkpointing"""
         
@@ -88,12 +91,12 @@ class MovieRecommenderModel:
 
         # Train the model
         history = self.model.fit(
-            x=X_train,
+            x=X_train_array,
             y=y_train,
             batch_size=batch_size,
             epochs=epochs,
             verbose=1,
-            validation_data=(X_val, y_val),
+            validation_data=(X_test_array, Y_test),
             shuffle=True,
             callbacks=[reduce_lr, checkpoint]
         )
@@ -115,7 +118,4 @@ class MovieRecommenderModel:
         predicted_ratings = np.max(predicted_ratings, axis=1)
         sorted_index = np.argsort(predicted_ratings)[::-1]
         recommended_movies = self.movie_enc.inverse_transform(sorted_index)
-        print("---------------------------------------------------------------------------------")
-        print("Top "+str(n_movies)+" Movie recommendations for the User "+str(user_id)+ " are:")
-        pprint(list(recommended_movies[:n_movies]))
         return list(recommended_movies[:n_movies])